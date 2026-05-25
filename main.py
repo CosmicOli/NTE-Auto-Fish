e6905490f9a9dd653bd3c0d5863f0e8f0069a217
noogoo1st <knightjulian42@gmail.com>
2026-05-25 08:45:54 +0100

    fixed to standard oython formating (i didnt want to but my vscode just does that so)

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from mss import mss
 import time
 import mouse
 
-boundingBox = {'top': 90, 'left': 810, 'width': 950, 'height': 20}
+boundingBox = {"top": 90, "left": 810, "width": 950, "height": 20}
 
 geen = (44, 205, 175)
 yellow = (254, 247, 166)
@@ -36,68 +36,67 @@ while True:
 
         colour = black
 
-        if (all(map(lambda x, y: abs((x - y)) / 255 <= 0.1, pixel, geen))):
-            if (firstGreenIndex == -1):
+        if all(map(lambda x, y: abs((x - y)) / 255 <= 0.1, pixel, geen)):
+            if firstGreenIndex == -1:
                 firstGreenIndex = currentPixelIndex
             lastGreenIndex = currentPixelIndex
 
             colour = geen
 
-        if (all(map(lambda x, y: abs((x - y)) / 255 <= 0.1, pixel, yellow))):
-            if (firstYellowIndex == -1):
+        if all(map(lambda x, y: abs((x - y)) / 255 <= 0.1, pixel, yellow)):
+            if firstYellowIndex == -1:
                 firstYellowIndex = currentPixelIndex
 
-            if (pixel == geen):
+            if pixel == geen:
                 colour = red
             else:
                 colour = yellow
 
         pixelArray.append(colour)
 
-    #print(pixelArray)
+    # print(pixelArray)
 
-# and all(map(lambda x: x >= 0, [firstGreenIndex, lastGreenIndex, currentYellowIndex]))
+    # and all(map(lambda x: x >= 0, [firstGreenIndex, lastGreenIndex, currentYellowIndex]))
 
-    #print([firstGreenIndex, lastGreenIndex, firstYellowIndex])
+    # print([firstGreenIndex, lastGreenIndex, firstYellowIndex])
 
     averageGreenIndex = (firstGreenIndex + lastGreenIndex) / 2
 
-    #print(averageGreenIndex)
+    # print(averageGreenIndex)
 
-    if (all(map(lambda x: x >= 0, [firstGreenIndex, lastGreenIndex, firstGreenIndex]))):
+    if all(map(lambda x: x >= 0, [firstGreenIndex, lastGreenIndex, firstGreenIndex])):
         spammingF = False
-        if (firstYellowIndex < averageGreenIndex):
-            keyboard.release('a')
-            keyboard.press('d')
+        if firstYellowIndex < averageGreenIndex:
+            keyboard.release("a")
+            keyboard.press("d")
             press = True
-            #print("d")
-        elif (firstYellowIndex > averageGreenIndex):
-            keyboard.release('d')
-            keyboard.press('a')
+            # print("d")
+        elif firstYellowIndex > averageGreenIndex:
+            keyboard.release("d")
+            keyboard.press("a")
             press = True
-            #print("a")
+            # print("a")
         else:
-            #print("TEST")
+            # print("TEST")
             press = False
     elif spammingF:
-        keyboard.press_and_release('f')
+        keyboard.press_and_release("f")
     else:
         time.sleep(5)
         mouse.move(50, 50)
         mouse.click("left")
         spammingF = True
 
-
-    #print(press)
+    # print(press)
 
     if press == False:
-        keyboard.release('d')
-        keyboard.release('a')
+        keyboard.release("d")
+        keyboard.release("a")
 
     img = Image.frombytes("RGB", screenshot.size, screenshot.bgra, "raw", "RGBX")
 
     img.putdata(pixelArray * 10)
 
-    cv2.imshow('capture', np.array(img))
+    cv2.imshow("capture", np.array(img))
 
     cv2.waitKey(1)
